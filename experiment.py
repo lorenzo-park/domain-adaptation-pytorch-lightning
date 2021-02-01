0818880f348465fe54b00c42cb6e7c61e5f96ebe
lorenzo@local <lorenzopark@postech.ac.kr>
2021-02-01 02:37:32 +0000

    split util functions and fix seed

--- experiment.py
+++ experiment.py
@@ -1,44 +1,8 @@
-from model.lenet import LeNet
-from model.svhn_cnn import SVHNCNN
-from pl_model.dann import DANN
-from pl_model.source_only import SO
+from util import get_img_size_from_source_dataset, get_baseline_model, get_da_model
 
 import argparse
 import pytorch_lightning as pl
 
-
-def get_img_size_from_source_dataset(src):
-    if src == "mnist":
-        return 28
-    
-    # Default img_size is 32
-    return 32
-
-
-def get_baseline_model(src):
-    if src == "mnist":
-        return LeNet()
-    
-    if src == "svhn":
-        return SVHNCNN()
-    
-    return None
-
-
-def get_da_model(model, base_model, params):
-    if model == "dann":
-        return DANN(
-            model=base_model, 
-            params=params
-        )
-    
-    if model == "so" or model == "source_only":
-        return SO(
-            model=base_model, 
-            params=params
-        )
-    
-    return None
 
 
 parser = argparse.ArgumentParser(description="Paper experiment results reproduction")
@@ -57,8 +21,11 @@ params = {
     "img_size": get_img_size_from_source_dataset(args.src),
     "gamma": 10,
     "alpha": 10,
-    "beta": 0.75
+    "beta": 0.75,
+    "seed": 42
 }
+
+pl.seed_everything(params["seed"])
 
 logger = pl.loggers.TestTubeLogger("output", name=f"{params['src']}2{params['tgt']}")
 logger.log_hyperparams(params)
